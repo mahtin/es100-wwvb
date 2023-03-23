4e0d34bc82d1948b64f458ff4b53a01576079bc6
Martin Levy <mahtin@mahtin.com>
2023-03-23 12:26:02 -0700

    0.4.2

---
+++ es100/__init__.py
@@ -0,0 +1,10 @@
+"""__init__
+
+Copyright (C) 2023 Martin J Levy - W6LHI/G8LHI - @mahtin - https://github.com/mahtin
+"""
+
+from .es100 import ES100, ES100Error
+
+__version__ = '0.4.2'
+
+__all__ = ['ES100', 'ES100Error']
