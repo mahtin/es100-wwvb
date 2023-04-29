5bb3b6746625702606a418b662dcf2097e0d3819
Martin Levy <mahtin@mahtin.com>
2023-04-29 17:21:55 +0100

    first pass with new pc board and its four leds

--- pico/board_led.py
+++ pico/board_led.py
@@ -7,8 +7,19 @@ Copyright (C) 2023 Martin J Levy - W6LHI/G8LHI - @mahtin - https://github.com/ma
 
 from machine import Pin
 
-led = Pin("LED", Pin.OUT) # internal LED
-led.off()
+ledINT = Pin('LED', Pin.OUT) # internal on board LED
+led16 = Pin('GP16', Pin.OUT) # external LED built into pc board
+led17 = Pin('GP17', Pin.OUT) # external LED built into pc board
+led18 = Pin('GP18', Pin.OUT) # external LED built into pc board
+led19 = Pin('GP19', Pin.OUT) # external LED built into pc board
+
+ledINT.off()
+led16.off()
+led17.off()
+led17.off()
+led18.off()
+
+led = led16 # presently we used this LED
 
 def led_on():
     led.on()
